21f6d374304fd0f803768bf9c12ab23f6065acc4
retobots <retobots.contact@gmail.com>
2025-05-20 12:34:30 +0700

    re-structure source code

--- 2d_plotter/src/main.cpp
+++ 2d_plotter/src/main.cpp
@@ -1,11 +1,11 @@
-#include "Arduino.h"
+#include "Controller.hpp"  // lớp trung tâm của Application Layer
 
-void setup()
-{
+Controller app;  // Khởi tạo controller chính (có thể là singleton)
 
+void setup() {
+    app.init();  // Khởi tạo toàn bộ hệ thống: LCD, PS4, Stepper, v.v.
 }
 
-void loop()
-{
-    
+void loop() {
+    app.run();   // Gọi luồng xử lý chính (chọn mode, điều khiển, vẽ G-code)
 }
