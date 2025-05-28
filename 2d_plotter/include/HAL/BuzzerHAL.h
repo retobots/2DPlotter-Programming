71802cd99d5c92c6d994bf5d5100c9d585b28327
retobots <retobots.contact@gmail.com>
2025-05-28 14:02:11 +0700

    restructure source code

---
+++ 2d_plotter/include/HAL/BuzzerHAL.h
@@ -0,0 +1,18 @@
+// #ifndef BuzzerHAL_H
+// #define BuzzerHAL_H
+
+// class BuzzerHAL {
+// public:
+//     BuzzerHAL();               // Constructor
+//     void init();                 // Hàm khởi tạo phần cứng hoặc logic
+//     void update();               // Gọi trong loop (nếu cần)
+    
+//     // Các method công khai khác
+//     void doSomething();
+    
+// private:
+//     // Biến nội bộ, trạng thái...
+//     int internalState;
+// };
+
+// #endif
