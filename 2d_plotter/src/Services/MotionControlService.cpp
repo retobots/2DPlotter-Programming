8b8faf47fbf7a76a16388ce401c0e78e0ac9e931
Khanh <90243049+ErikDrake2005@users.noreply.github.com>
2025-06-11 01:13:06 +0700

    Update MotionControlService.cpp (developing)

--- 2d_plotter/src/Services/MotionControlService.cpp
+++ 2d_plotter/src/Services/MotionControlService.cpp
@@ -1,20 +1,14 @@
-#include "Services/MotionControlService.h"
-
-MotionControlService::MotionControlService() {}
-
-MotionControlService& MotionControlService::getInstance() {
-  static MotionControlService instance;
-  return instance;
+#include "MotionControlService.h"
+MotionControlService::MotionControlService(){}
+MotionControlService& MotionControlService::getInstance(){
+	static MotionControlService instance;
+	return instance; }
+void MotionControlService::startManualMode(){
+	// Code Manual Mode
 }
-
-void MotionControlService::setup() {
-  // Khởi tạo stepper nếu cần
+void MotionControlService::loadFromSDCard(){
+	// Code SD Mode
 }
-
-void MotionControlService::moveTo(float x, float y) {
-  // Tính toán và điều khiển di chuyển stepper
-}
-
-void MotionControlService::stop() {
-  // Dừng mọi chuyển động
+void MotionControlService::connectSerial(){
+	// Code Serial Mode
 }
