0f7a634a4779e11ba387e739469d26fdf6ad6402
Đỗ Đức Nghĩa <dungnhannghia2605@gmail.com>
2025-11-28 08:45:54 +0700

    Update

--- 2d_plotter/src/main.cpp
+++ 2d_plotter/src/main.cpp
@@ -8,30 +8,22 @@ void homeMove()
   Serial.println("[PROCESS]: Starting Homing Sequence");
 
   // Set speed for homing
-  IoHwAb_Stepper::getInstance().setSpeed(400); // Set a slower speed for homing
-
-  // x steps, y steps
-  float x = 0.0; // Move negative direction
-  float y = 0.0; // Move negative direction
+  IoHwAb_Stepper::getInstance().setSpeed(-400); // Set a slower speed for homing
 
   // Di chuyển trục Y về vị trí công tắc hành trình
   while (digitalRead(PIN_ENDSTOP_Y) == HIGH)
   {
-    y -= 10.0;
-    float ySteps = y * STEPS_PER_MM_Y;
-    IoHwAb_Stepper::getInstance().moveTo(0, ySteps); // Di chuyển ngược trục Y
+    IoHwAb_Stepper::getInstance().runSpeedTick(); // Di chuyển ngược trục Y
     Serial.println("[PROCESS]: Homing Y Axis...");
   }
   IoHwAb_Stepper::getInstance().setCurrentPosition(0, 0);
-  IoHwAb_Stepper::getInstance().moveTo(0, 0); // rất quan trọng
+  IoHwAb_Stepper::getInstance().moveTo(0, 0);
   Serial.println("[PROCESS]: Y Axis Homed");
 
   // Di chuyển trục X về vị trí công tắc hành trình
   while (digitalRead(PIN_ENDSTOP_X) == HIGH)
   {
-    x -= 10.0;
-    float xSteps = x * STEPS_PER_MM_X;
-    IoHwAb_Stepper::getInstance().moveTo(xSteps, 0); // Di chuyển ngược trục X
+    IoHwAb_Stepper::getInstance().runSpeedTick(); // Di chuyển ngược trục X
     Serial.println("[PROCESS]: Homing X Axis...");
   }
   IoHwAb_Stepper::getInstance().setCurrentPosition(0, 0);
